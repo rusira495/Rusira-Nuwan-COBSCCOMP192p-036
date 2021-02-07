18f0d068e48c5b059984f0571a711367b6500a94
wincal <rusirarh25@gmail.com>
2021-02-07 23:19:32 +0530

    Initial Commit

---
+++ WinWinCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WinWinCalculator
+//
+//  Created by Avishka Balasuriya on 2021-02-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
